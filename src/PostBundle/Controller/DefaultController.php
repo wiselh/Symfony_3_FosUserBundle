eb91fc3d3ed7afb58466b5695e77852e098687ea
wiselh <wise.self.2@gmail.com>
2018-04-27 12:32:38 +0100

    Install JmsSerializer
    $ composer require jms/serializer-bundle

--- src/PostBundle/Controller/DefaultController.php
+++ src/PostBundle/Controller/DefaultController.php
@@ -2,16 +2,48 @@
 
 namespace PostBundle\Controller;
 
+use PostBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/posts", name="posts")
      */
     public function indexAction()
     {
-        return $this->render('PostBundle:Default:index.html.twig');
+        $posts =$this->getDoctrine()->getRepository(Post::class)->findAll();
+
+        if (!count($posts)){
+            $response=array(
+
+                'code'=>1,
+                'message'=>'No posts found!',
+                'errors'=>null,
+                'result'=>null
+
+            );
+
+
+            return new JsonResponse($response, Response::HTTP_NOT_FOUND);
+        }
+
+
+        $data=$this->get('jms_serializer')->serialize($posts,'json');
+
+        $response=array(
+
+            'code'=>0,
+            'message'=>'success',
+            'errors'=>null,
+            'result'=>json_decode($data)
+
+        );
+
+        return new JsonResponse($response,200);
+//        return $this->render('PostBundle:Default:index.html.twig');
     }
 }
