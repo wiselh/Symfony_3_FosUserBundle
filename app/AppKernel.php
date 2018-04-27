605152a2bb5cf7450e96cb80029e5a4f40e15e55
wiselh <wise.self.2@gmail.com>
2018-04-27 17:46:32 +0100

    Install for Angularjs or any other javascript frameworks
    $ composer require nelmio/cors-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new PostBundle\PostBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
