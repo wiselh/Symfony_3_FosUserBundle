80db7ab55c7d57bce667d9c9210c96fb0ff99bc6
wiselh <wise.self.2@gmail.com>
2018-04-27 12:05:10 +0100

    Generate PostBundle & Post Entity
    $ php bin/console generate:bundle
    Update composer.json : $ composer dump-autoload
    $ php bin/console doctrine:generate:entity

---
+++ src/PostBundle/Entity/Post.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace PostBundle\Entity;
+
+/**
+ * Post
+ */
+class Post
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $titre;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Post
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Post
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
